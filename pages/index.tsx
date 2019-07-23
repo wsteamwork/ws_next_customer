62369c54553a4fc824547cba2c33642674d2a6aa
Đào Duy <duydtd97@gmail.com>
2019-07-23 15:27:55 +0700

    duy PaymentContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,21 @@ import { NextPage } from 'next';
 import NextHead from '@/components/NextHead';
 import Link from 'next/link';
 
-const Home: NextPage = () => (
-  <div>
-    <NextHead
-      title="Nextjs Demo"
-      description="Welcome to Nextjs"
-      url="https://nextjs.org/"></NextHead>
+const Home: NextPage = () => {
+  return(
+    <div>
+      <NextHead
+        title="Nextjs Demo"
+        description="Welcome to Nextjs"
+        url="https://nextjs.org/"/>
 
-    <div style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Hello NextJS 😄</h1>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <div style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <h1>Hello NextJS 😄</h1>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  )
+};
 export default Home;
