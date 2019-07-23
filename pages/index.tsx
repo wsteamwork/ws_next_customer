2b2b55e3cb0aa5b3d58f82a5e3e142a316353ec7
dat20897 <datfc97pro@gmail.com>
2019-07-23 13:57:31 +0700

    update 1 7/23/2019

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { NextPage } from 'next';
 import NextHead from '@/components/NextHead';
 import Link from 'next/link';
+import { useTranslation, UseTranslationResponse } from 'react-i18next';
 
-const Home: NextPage = () => (
-  <div>
-    <NextHead
-      title="Nextjs Demo"
-      description="Welcome to Nextjs"
-      url="https://nextjs.org/"></NextHead>
+const Home: NextPage = () => {
+  const { t }: UseTranslationResponse = useTranslation();
 
-    <div style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Hello NextJS 😄</h1>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+  return (
+    <div>
+      <NextHead
+        title="Nextjs Demo"
+        description="Welcome to Nextjs"
+        url="https://nextjs.org/"></NextHead>
+
+      <div style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <h1>{t('example')} 😄</h1>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
--- utils/ProviderGlobal/index.tsx
+++ utils/ProviderGlobal/index.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, Suspense } from 'react';
 import { compose } from 'recompose';
 import withWidth from '@material-ui/core/withWidth';
 import { CookiesProvider } from 'react-cookie';
 import { withRouter } from 'next/router';
 import { GlobalContext, IGlobalContext } from '@/store/Context/GlobalContext';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/translations/index';
 
 interface IProps extends IGlobalContext {}
 
@@ -11,9 +13,13 @@ const ProviderGlobal: FC<IProps> = (props) => {
   const { width, router, children } = props;
 
   return (
-    <CookiesProvider>
-      <GlobalContext.Provider value={{ width, router }}>{children}</GlobalContext.Provider>
-    </CookiesProvider>
+    <Suspense fallback={null}>
+      <CookiesProvider>
+        <I18nextProvider i18n={i18n}>
+          <GlobalContext.Provider value={{ width, router }}>{children}</GlobalContext.Provider>
+        </I18nextProvider>
+      </CookiesProvider>
+    </Suspense>
   );
 };
 
