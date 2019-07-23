fe9ced73079e07164958baa5b810b214f1b517f6
arima <tronghung199@gmail.com>
2019-07-23 11:44:22 +0700

    init

---
+++ store/Context/Profile/ProfileContext.ts
@@ -0,0 +1,100 @@
+import { createContext, Dispatch, useContext } from 'react';
+import { ProfileInfoRes } from '@/types/Requests/Profile/ProfileResponse';
+import { BookingIndexRes } from '@/types/Requests/Booking/BookingResponses';
+import { updateObject } from '@/store/Context/utility';
+import { AxiosRes, Pagination } from '@/types/Requests/ResponseTemplate';
+import { axios } from '@/utils/axiosInstance';
+import { BookingIndexParams } from '@/types/Requests/Booking/BookingRequests';
+//ts-ignore
+import qs from 'query-string';
+import { AxiosError } from 'axios';
+
+export interface IProfileContext {
+  state: ProfileState;
+  dispatch: Dispatch<ProfileAction>;
+}
+
+export type ProfileAction =
+  | {
+      type: 'setData';
+      profile?: ProfileInfoRes;
+      bookings?: BookingIndexRes[];
+      meta?: Pagination;
+    }
+  | { type: 'setDataBooking'; bookings?: BookingIndexRes[]; meta?: Pagination };
+
+export type ProfileState = {
+  readonly profile?: ProfileInfoRes | null;
+  readonly bookings: BookingIndexRes[];
+  readonly metaBookings?: Pagination;
+};
+
+export const ProfileStateInit: ProfileState = {
+  profile: null,
+  bookings: []
+};
+
+export const ProfileReducer = (state: ProfileState, action: ProfileAction) => {
+  switch (action.type) {
+    case 'setData':
+      return updateObject<ProfileState>(state, {
+        profile: action.profile,
+        bookings: action.bookings,
+        metaBookings: action.meta
+      });
+    case 'setDataBooking':
+      return updateObject<ProfileState>(state, {
+        bookings: action.bookings,
+        metaBookings: action.meta
+      });
+    default:
+      return state;
+  }
+};
+
+export const getProfile = async (token?: any) => {
+  const headers = token && {
+    headers: {
+      Authorization: token
+    }
+  };
+
+  try {
+    const res: AxiosRes<ProfileInfoRes> = await axios.get('profile?include=city,district', headers);
+    return res.data;
+  } catch (err) {
+    console.log(err.response);
+  }
+};
+
+export const getUserBookingList = async (status?: number, page?: number) => {
+  const params: Partial<BookingIndexParams> = {
+    status,
+    include: 'room.details,room.media',
+    size: 5,
+    page: page
+  };
+  const res: AxiosRes<BookingIndexRes[]> = await axios.get(`bookings?${qs.stringify(params)}`);
+  return res.data;
+};
+
+export const getDataProfile = (
+  dispatch: Dispatch<ProfileAction>,
+  status?: number,
+  page?: number
+) => {
+  Promise.all([getProfile(), getUserBookingList(status, page)])
+    .then((res) => {
+      const [profile, bookings] = res;
+
+      dispatch({
+        type: 'setData',
+        profile: profile!.data,
+        bookings: bookings.data,
+        meta: bookings.meta
+      });
+    })
+    .catch((err: AxiosError) => {
+      console.log(err);
+    });
+};
---
+++ store/Context/Profile/ProfileViewContext.tsx
@@ -0,0 +1,89 @@
+import { createContext, Dispatch } from "react";
+import { ProfileViewInfoRes } from "@/types/Requests/Profile/ProfileResponse";
+import { updateObject } from '@/store/Context/utility';
+import { AxiosRes, BaseResponse } from "@/types/Requests/ResponseTemplate";
+import { axios } from "@/utils/axiosInstance";
+// import { History } from "history";
+import { AxiosError } from "axios";
+// import {fetchRoom} from '@/store/context/Room/RoomIndexContext';
+import _ from "lodash";
+import { RoomIndexRes } from "@/types/Requests/Rooms/RoomResponses";
+import { NextRouter } from "next/router";
+
+export const ProfileViewContext = createContext<IProfileViewContext | any>(null);
+
+export interface IProfileViewContext {
+  state: ProfileViewState;
+  dispatch: Dispatch<ProfileViewAction>;
+}
+
+export type ProfileViewAction = {
+  type: "setData";
+  profile?: ProfileViewInfoRes;
+  userRooms: RoomIndexRes[];
+} | { type: 'setUserRooms', rooms: RoomIndexRes[] }
+
+export type ProfileViewState = {
+  readonly profile?: ProfileViewInfoRes | null;
+  readonly userRooms?: RoomIndexRes[] 
+};
+
+export const ProfileViewStateInit: ProfileViewState = {
+  profile: null,
+  userRooms: []
+};
+
+export const ProfileViewReducer = (
+  state: ProfileViewState,
+  action: ProfileViewAction
+) => {
+  switch (action.type) {
+    case "setData":
+      return updateObject<ProfileViewState>(state, {
+        profile: action.profile,
+        userRooms: action.userRooms
+      });
+      case 'setUserRooms':
+        return updateObject<ProfileViewState>(state, {
+          userRooms: action.rooms
+        })
+    default:
+      return state;
+  }
+};
+
+export const getProfileById = async (idUser: number) => {
+  const res: AxiosRes<ProfileViewInfoRes> = await axios.get(
+    `/profile/${idUser}`
+  );
+  return res.data;
+};
+
+// export const getRoomMerchantById = async (idUser: Number): Promise<BaseResponse<RoomIndexRes[]>> => {
+//   return fetchRoom(`rooms?merchant=${idUser}&include=reviews.user,details,media,city,district`);
+// }
+
+export const getDataProfile = (
+  id: number,
+  dispatch: Dispatch<ProfileViewAction>,
+  router: NextRouter
+) => {
+  if (isNaN(id)) router.push("/");
+
+  Promise.all([
+    getProfileById(id),
+    // getRoomMerchantById(id)
+  ])
+    .then(res => {
+      const [user, userRooms] = res;
+      dispatch({
+        type: "setData",
+        profile: user.data,
+        // userRooms: userRooms.data
+      });
+    })
+    .catch((err: AxiosError) => {
+      console.log(err)
+      router.push('/404')
+    });
+};
