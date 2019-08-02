01fdab5087782f62af40a09fdac11714f7254eec
Tuan Anh <tuananhpham1402@gmail.com>
2019-08-02 17:27:21 +0700

    fix avg_price

--- components/Room/BoxBooking/PriceDetail/index.tsx
+++ components/Room/BoxBooking/PriceDetail/index.tsx
@@ -13,29 +13,29 @@ const PriceDetail: FC = () => {
     () =>
       room && (
         <Grid container className="priceDetail">
-          {!!dataCalculate && dataCalculate.days > 5 ? (
+          {!!dataCalculate && dataCalculate.days >= 5 ? (
             <Grid className="priceDetail__avg">
               <p>
-                {t('room:boxBooking:average')} {numeral(room.price_day).format('0,0')} VND/{' '}
+                {t('room:boxBooking:average')} {numeral(dataCalculate.avg_price).format('0,0')} VND/{' '}
                 {t('room:boxBooking:day')}
               </p>
             </Grid>
           ) : (
-            <Grid container>
-              <Grid item xs={6} className="priceDetail__priceDay flex_center">
-                <p>
-                  {numeral(room.price_day).format('0,0')} VND/ {t('room:boxBooking:day')}
-                </p>
-              </Grid>
-              <Grid item xs={6} className="priceDetail__priceHour flex_center">
-                {room.price_hour !== 0 && (
+              <Grid container>
+                <Grid item xs={6} className="priceDetail__priceDay flex_center">
                   <p>
-                    {numeral(room.price_hour).format('0,0')} VND/ 4 {t('room:boxBooking:hours')}
+                    {numeral(room.price_day).format('0,0')} VND/ {t('room:boxBooking:day')}
                   </p>
-                )}
+                </Grid>
+                <Grid item xs={6} className="priceDetail__priceHour flex_center">
+                  {room.price_hour !== 0 && (
+                    <p>
+                      {numeral(room.price_hour).format('0,0')} VND/ 4 {t('room:boxBooking:hours')}
+                    </p>
+                  )}
+                </Grid>
               </Grid>
-            </Grid>
-          )}
+            )}
         </Grid>
       ),
     [t, room, dataCalculate]
