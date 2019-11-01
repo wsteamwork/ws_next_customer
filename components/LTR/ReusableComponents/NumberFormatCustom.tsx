49268967ebc5df5b1552f8d3c37474f8f8ece7da
arima <tronghung199@gmail.com>
2019-11-01 17:30:31 +0700

    update

--- components/LTR/ReusableComponents/NumberFormatCustom.tsx
+++ components/LTR/ReusableComponents/NumberFormatCustom.tsx
@@ -1,40 +1,41 @@
 import { Theme } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import React, { FC } from 'react';
-import NumberFormat from "react-number-format";
+import NumberFormat from 'react-number-format';
 
 interface NumberFormatCustomProps {
-  classes?: any
+  classes?: any;
   inputRef?: (instance: NumberFormat | null) => void;
   onChange?: (event: { target: { value: string } }) => void;
+  haveThousandSeparator: boolean;
 }
 
-const useStyles = makeStyles<Theme, NumberFormatCustomProps>((theme: Theme) =>
-  createStyles({
-
-  })
-);
+const useStyles = makeStyles<Theme, NumberFormatCustomProps>((theme: Theme) => createStyles({}));
 
 const NumberFormatCustom: FC<NumberFormatCustomProps> = (props) => {
   const classes = useStyles(props);
-  const { inputRef, onChange, ...other } = props;
+  const { inputRef, onChange, haveThousandSeparator, ...other } = props;
 
   return (
     <NumberFormat
       {...other}
       allowNegative={false}
       getInputRef={inputRef}
-      onValueChange={values => {
+      onValueChange={(values) => {
         onChange({
           target: {
-            value: values.value,
-          },
+            value: values.value
+          }
         });
       }}
-      thousandSeparator
+      thousandSeparator={haveThousandSeparator}
       allowEmptyFormatting={false}
     />
   );
+};
+
+NumberFormatCustom.defaultProps = {
+  haveThousandSeparator: true
 };
 
 export default NumberFormatCustom;
